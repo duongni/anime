7cd647a93f985462fd714de9194532a38532f9d9
duongni <133175915+duongni@users.noreply.github.com>
2024-01-31 20:48:48 -0500

    new anime website project

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="bg-hero bg-cover bg-center h-[762px] max-w-[1280px] flex justify-center items-center">
+      Hero
+    </div>
+  );
+};
+
+export default Hero;
